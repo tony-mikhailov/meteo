f6120e985f63b963b999f416673de166350f84f9
Tony Rasskazov <tony.rasskazov@gmail.com>
2016-04-09 06:24:04 +0300

    write to data file and try to search tty

--- web/serialworker.py
+++ web/serialworker.py
@@ -5,7 +5,8 @@ import multiprocessing
 ## Change this to match your local settings
 SERIAL_BAUDRATE = 9600
 #SERIAL_PORT = '/dev/tty.usbmodem1411'
-SERIAL_PORT = '/dev/ttyACM0'
+SERIAL_PORT = ['/dev/ttyACM0',  '/dev/ttyACM1', '/dev/ttyACM2']
+CS = 0
 
 class SerialProcess(multiprocessing.Process):
  
@@ -13,12 +14,14 @@ class SerialProcess(multiprocessing.Process):
         multiprocessing.Process.__init__(self)
         self.input_queue = input_queue
         self.output_queue = output_queue
+        global CS
         try:
-            self.sp = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE, timeout=1)
+            self.sp = serial.Serial(SERIAL_PORT[CS], SERIAL_BAUDRATE, timeout=1)
             self.sp.flushInput()
-            print "connected to " + SERIAL_PORT        
+            print "connected to " + SERIAL_PORT[CS]        
         except:
-            print "couldn`t conect to " + SERIAL_PORT        
+            print "couldn`t conect to " + SERIAL_PORT[CS]
+            CS = CS + 1        
  
     def close(self):
         self.sp.close()
@@ -32,6 +35,7 @@ class SerialProcess(multiprocessing.Process):
  
     def run(self):
         self.output_queue.put("()");
+        global CS
         while True:
             if not self.input_queue.empty():
                 data = self.input_queue.get()
@@ -45,9 +49,11 @@ class SerialProcess(multiprocessing.Process):
             except:
                 self.output_queue.put("()");
                 try:
-                    self.sp = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE, timeout=1)
+                    self.sp = serial.Serial(SERIAL_PORT[CS], SERIAL_BAUDRATE, timeout=1)
                     self.sp.flushInput()
-                    print "connected to " + SERIAL_PORT        
+                    print "connected to " + SERIAL_PORT[CS]        
                 except:
-                    print "couldn`t conect to " + SERIAL_PORT        
+                    print "couldn`t conect to " + SERIAL_PORT[CS]
+                    CS = CS + 1
+                    if CS >= 3: CS = 0        
                     time.sleep(1)
--- web/server.py
+++ web/server.py
@@ -45,6 +45,35 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 def checkQueue():
 	if not output_queue.empty():
 		message = output_queue.get()
+                msg = message[1:-2]
+                print msg
+                T = -1.0
+                H  = -1.0
+                P = -1.0
+                windSpeed = -1.0
+                windDir = -1.0
+                try:
+                    windSpeed = msg.split(' ')[3]
+                    rps = msg.split(' ')[5]
+                    a0 = msg.split(' ')[11]
+                    a0f = float(a0)
+                    windDir = 1.5 + (358.5 - 1.5)  * (a0f - 500.0) / (1012.0 - 500.0);
+                    P = msg.split(' ')[13]
+                    H = msg.split(' ')[15]
+                    T = msg.split(' ')[17]
+                finally:
+                    print "outTemp=" + str(T)
+                    print "barometer=" + str(P)
+                    print "outHumidity=" + str(H)
+                    print "windSpeed=" + str(windSpeed)
+                    print "windDir=" + str(windDir)
+                    f = open('datafile', 'w')
+                    f.write("outTemp=" + str(T) + '\n')
+                    f.write("barometer=" + str(P) + '\n')
+                    f.write("outHumidity=" + str(H) + '\n')
+                    f.write("windSpeed=" + str(windSpeed) + '\n')
+                    f.write("windDir=" + str(windDir) + '\n')
+ 
 		for c in clients:
 			c.write_message(message)
 
@@ -70,7 +99,7 @@ if __name__ == '__main__':
 
 	mainLoop = tornado.ioloop.IOLoop.instance()
 
-	scheduler_interval = 100
+	scheduler_interval = 1000
 	scheduler = tornado.ioloop.PeriodicCallback(checkQueue, scheduler_interval, io_loop = mainLoop)
 	scheduler.start()
 	mainLoop.start()
